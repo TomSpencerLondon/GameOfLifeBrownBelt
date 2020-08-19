b601950d1faf95fc3bb2f8d6327e8af24876c6cf
Tom <thomas.spencer@codurance.com>
2020-08-19 16:00:30 +0100

    Move tests

--- src/test/java/com/codurance/CellShould.java
+++ src/test/java/com/codurance/CellShould.java
@@ -31,6 +31,24 @@ public class CellShould {
     assertEquals(CellState.DEAD, result);
   }
 
+  @Test
+  void become_alive_with_three_neighbours() {
+    Cell cell = new Cell(CellState.DEAD);
+
+    CellState result = cell.getNextState(3);
+
+    assertEquals(CellState.ALIVE, result);
+  }
+
+  @Test
+  void stay_dead_with_two_neighbours() {
+    Cell cell = new Cell(CellState.DEAD);
+
+    CellState result = cell.getNextState(2);
+
+    assertEquals(CellState.DEAD, result);
+  }
+
   @ParameterizedTest
   @CsvSource({
       "2",
@@ -42,23 +60,6 @@ public class CellShould {
     CellState result = cell.getNextState(neighbours);
 
     assertEquals(CellState.ALIVE, result);
-  }
-
-  @Test
-  void dead_should_become_alive_with_three_neighbours() {
-    Cell cell = new Cell(CellState.DEAD);
-
-    CellState result = cell.getNextState(3);
-
-    assertEquals(CellState.ALIVE, result);
-  }
-  @Test
-  void dead_stays_dead_with_two_neighbours() {
-    Cell cell = new Cell(CellState.DEAD);
-
-    CellState result = cell.getNextState(2);
-
-    assertEquals(CellState.DEAD, result);
   }
 
   @ParameterizedTest
